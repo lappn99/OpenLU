e80ccb031e786566c61f4fdbfe6d96ae3852bf82
MashedTatos <32725020+MashedTatos@users.noreply.github.com>
2020-08-14 22:02:37 -0400

    Add project files.

---
+++ OpenLU/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+open System.Threading
+open LegoUniverse.Networking
+[<EntryPoint>]
+let main argv =
+   let server = Server(1001)
+   server.StartServer()
+   Console.ReadKey() |> ignore
+   0
