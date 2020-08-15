a93525ef0bf3c03aaf55296e1da8132e1251768a
MashedTatos <32725020+MashedTatos@users.noreply.github.com>
2020-08-15 16:30:50 -0400

    Removed uncessary imports

--- OpenLU/Program.fs
+++ OpenLU/Program.fs
@@ -1,13 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open System.IO
-open System.Net
-open System.Net.Sockets
-open System.Threading
 open OpenLU.Core
 open OpenLU.Auth
-open OpenLU.Services
 [<EntryPoint>]
 let main argv =
     let lu = LegoUniverse(AuthServer())
