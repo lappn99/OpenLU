c1b8627568e7a6eee26dc828c696fa0299de8429
MashedTatos <32725020+MashedTatos@users.noreply.github.com>
2020-08-17 20:17:24 -0400

    Moved ConfigurationProvider to seperate project

--- OpenLU.Configuration/ConfigurationProvider.fs
+++ OpenLU.Configuration/ConfigurationProvider.fs
@@ -1,10 +1,11 @@
-﻿namespace OpenLU
+﻿namespace OpenLU.Configuration
 open Microsoft.Extensions.Configuration
-module ConfigurationProvider = 
+
+module public ConfigurationProvider = 
     let private initialze() = 
         let create = fun() -> ConfigurationBuilder().AddJsonFile("cfg.json").Build()
         Lazy.Create(create)
     let mutable private currentConfig = initialze()
-    let GetConfig() = currentConfig.Value
+    let public GetConfig() = currentConfig.Value
         
 
--- OpenLU.DBContext/MySqlContext.cs
+++ OpenLU.DBContext/MySqlContext.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using OpenLU.Configuration;
 namespace OpenLU.DBContext
 {
     public class MySqlContext : BaseContext
@@ -10,7 +10,7 @@ namespace OpenLU.DBContext
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("server=localhost;database=openlu;user id=lappn;password=admin;persistsecurityinfo=True;port=3306");
+            optionsBuilder.UseMySql(ConfigurationProvider.GetConfig()["connectionString"]);
         }
     }
 }
